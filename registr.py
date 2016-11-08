43829e12c4e902318bbf60038f6ed23cca716a90
Hui Yiqun <huiyiqun@gmail.com>
2016-11-08 10:09:17 +0800

    add team

--- registr.py
+++ registr.py
@@ -28,6 +28,8 @@ class Candidate(EveMixin, Base):
     phone = Column(String(16), nullable=True)
     email = Column(String(120), nullable=True)
     gender = Column(Enum('female', 'male'), nullable=True)
+    team = Column(Enum('DevOps', 'Organizer', 'Publicity', 'Jiangyou'),
+                  nullable=True)
 
 
 def create_app():
